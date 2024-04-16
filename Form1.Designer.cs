1f2f4578b36f30d83c775c66fd8668b91fbd370f
LJ Salazar <122578041+codemorelj@users.noreply.github.com>
2024-04-17 04:03:30 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace GuessWordApplication
+{
+    partial class frmGuessWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.answer = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.showResult = new System.Windows.Forms.Panel();
+            this.wordz = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.showResult.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // answer
+            // 
+            this.answer.BackColor = System.Drawing.SystemColors.Menu;
+            this.answer.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answer.Location = new System.Drawing.Point(63, 123);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(172, 37);
+            this.answer.TabIndex = 1;
+            this.answer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.SystemColors.Control;
+            this.submitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.submitButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.submitButton.Location = new System.Drawing.Point(151, 208);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Padding = new System.Windows.Forms.Padding(2);
+            this.submitButton.Size = new System.Drawing.Size(84, 40);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "guess";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(328, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wrong guess";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 4;
+            // 
+            // showResult
+            // 
+            this.showResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.showResult.Controls.Add(this.label3);
+            this.showResult.Location = new System.Drawing.Point(318, 77);
+            this.showResult.Name = "showResult";
+            this.showResult.Size = new System.Drawing.Size(180, 196);
+            this.showResult.TabIndex = 5;
+            // 
+            // wordz
+            // 
+            this.wordz.AutoSize = true;
+            this.wordz.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wordz.Location = new System.Drawing.Point(93, 53);
+            this.wordz.Name = "wordz";
+            this.wordz.Size = new System.Drawing.Size(109, 44);
+            this.wordz.TabIndex = 6;
+            this.wordz.Text = "word";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(63, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "skip";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmGuessWord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 313);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.wordz);
+            this.Controls.Add(this.showResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.answer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmGuessWord";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess the Word";
+            this.showResult.ResumeLayout(false);
+            this.showResult.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox answer;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel showResult;
+        private System.Windows.Forms.Label wordz;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+    //AUTHOR: LJ Salazar || BSIT 202 C
+
+namespace GuessWordApplication
+{
+    public partial class frmGuessWord : Form
+    {
+        public frmGuessWord()
+        {
+            InitializeComponent();
+            displayWord();
+        }
+
+        static Random random = new Random();
+
+        //array for set of words na pwede hulaan
+        static string[] words =
+        {
+            "anger",
+            "charcoal",
+            "carbon",
+            "sofa",
+            "bottle",
+            "guess"
+        };
+
+        int index = random.Next(words.Length);
+
+        //for the underline/missing letters
+        public void displayWord()
+        {
+            int pos1 = random.Next(words[index].Length);
+            int pos2 = random.Next(words[index].Length);
+            int pos3 = random.Next(words[index].Length);
+
+            string word = words[index];
+            StringBuilder wordBuilder = new StringBuilder(word);
+
+            wordBuilder[pos1] = '_';
+            wordBuilder[pos2] = '_';
+            wordBuilder[pos3] = '_';
+
+            //StringBuilder class used hehe
+            wordz.Text = wordBuilder.ToString();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            string userinput = answer.Text.ToLower();
+
+            if (userinput.Equals(words[index]))
+            {
+                MessageBox.Show("Well done! You got it!", "Nice!");
+                index = random.Next(words.Length);
+                displayWord();
+                label3.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("You got it wrong :(", "Try again");
+                label3.Text += answer.Text + "\n";
+            }
+
+            //auto delete sa sagot every after submitting the answer
+            answer.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            index = random.Next(words.Length);
+            displayWord();
+            label3.Text = "";
+            answer.Clear();
+        }
+    }
+}
